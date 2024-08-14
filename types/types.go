b4ff7faea0c2c1c2b6afba9b7d7982e4f70c53ce
SpecTro <natplayspectro@gmail.com>
2024-08-14 09:47:13 -0300

    fix attribute declaration before struct

--- types/types.go
+++ types/types.go
@@ -1,10 +1,9 @@
 package types
 
-	Name string
 type Package struct { // TODO: make fields exported and immutable
+	Name       string
 	IsExplicit bool
-	Version string
-	Size uint
+	Version    string
+	Size       uint
 	Deps       []string // NOTE: change this to []*Package or map[string]*Packge ?
 }
-
