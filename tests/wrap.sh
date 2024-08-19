597e37ed44eb323c8ec182a28f8a614fee61ecb5
SpecTro <natplayspectro@gmail.com>
2024-08-19 15:03:53 -0300

    test(wrap.sh): minor refactoring

--- tests/wrap.sh
+++ tests/wrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASEDIR=$( cd "$(dirname "$0")"; cd ..; pwd ) # TODO: see if I can do this better
+BASEDIR=$( cd "$(dirname "$0")"/..; pwd )
 cd "$BASEDIR"
 
 # put pm_mock.sh into a bin dir for each package manager
